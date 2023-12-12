981f1f3c03e7de30291897b0ccb706facbe06c56
MdAkashSheikh <nmakashsheikh@gmail.com>
2023-12-12 18:48:26 +0600

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,6 +1,14 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+const mongoose  = require('mongoose');
 
-app.listen(5000, () => {
-    console.log('Listening on PORT 5000')
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.DB_URL)
+.then(() => console.log('Database Connected.'))
+.catch((err) => console.log(err));
+
+app.listen(PORT, () => {
+    console.log(`Listening on PORT ${PORT}`)
 })
