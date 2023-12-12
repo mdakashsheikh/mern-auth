77b976110b0ce7c7425bd75f262af6d930c1ff47
MdAkashSheikh <nmakashsheikh@gmail.com>
2023-12-12 19:50:28 +0600

    create test api route

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,9 @@
+const test = (req, res) => {
+    res.json({
+        message: 'API is Working Successfully'
+    })
+}
+
+module.exports = {
+    test,
+}
--- api/index.js
+++ api/index.js
@@ -2,12 +2,16 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose  = require('mongoose');
+const userRoutes = require('./routes/user.route')
 
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.DB_URL)
 .then(() => console.log('Database Connected.'))
 .catch((err) => console.log(err));
+
+
+app.use('/api/user', userRoutes)
 
 app.listen(PORT, () => {
     console.log(`Listening on PORT ${PORT}`)
