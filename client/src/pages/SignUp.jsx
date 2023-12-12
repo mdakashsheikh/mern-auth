c87ab43775c9420f7a82e97e44a3b50f1ceb60ce
MdAkashSheikh <nmakashsheikh@gmail.com>
2023-12-13 02:45:55 +0600

    complete sign in page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,11 +1,12 @@
 import axios from 'axios';
 import React, { useState } from 'react'
-import {Link} from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function SignUp() {
     const [formData, setFormData] = useState({});
     const [error, setError] = useState(false);
     const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
 
     const handleChange = (e) => {
         setFormData({...formData, [e.target.id]: e.target.value});
@@ -21,6 +22,7 @@ export default function SignUp() {
             const res = await axios.post('/api/auth/signup', formData);
             console.log(res.data);
             setLoading(false);
+            navigate('/sign-in')
 
         } catch (error) {
             setLoading(false);
