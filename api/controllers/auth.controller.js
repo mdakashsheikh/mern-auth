d2f65beb7b3e5b7220e4cfdea44055c8566146e2
MdAkashSheikh <nmakashsheikh@gmail.com>
2023-12-13 02:18:42 +0600

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,8 @@
+require('dotenv').config();
 const User = require("../models/user.model");
 const bcryptjs = require('bcryptjs');
+const errorHandler = require("../utils/error");
+const jwt = require('jsonwebtoken');
 
 const signup = async(req, res, next) => {
     console.log(req.body);
@@ -15,6 +18,30 @@ const signup = async(req, res, next) => {
     }
 }
 
+const signin = async(req, res, next) => {
+    const {email, password} = req.body;
+
+    try {
+        const validUser = await User.findOne({ email });
+        if(!validUser) return next(errorHandler(401, 'User not found'));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!validPassword) return next(errorHandler(401, 'Worng Credential'));
+
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        const { password: hashedPassword, ...rest} = validUser._doc;
+        const expiryDate = new Date(Date.now() + 3600000) // 1 hour
+
+        res
+            .cookie('access_token', token, { httpOnly: true, expires: expiryDate } )
+            .status(200)
+            .json(rest);
+
+    } catch (error) {
+        next(error);
+    }
+}
+
 module.exports = {
     signup,
+    signin,
 }
