c32111a099f08124b367075679b015d25c2d29e4
MdAkashSheikh <nmakashsheikh@gmail.com>
2023-12-18 19:51:44 +0600

    complete image upload functional

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,17 +1,91 @@
 import {useSelector} from 'react-redux';
+import { useEffect, useRef, useState } from 'react';
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import { app } from '../firebase';
 
 export default function Profile() {
+
+    const fileRef = useRef(null);
+    const [image, setImage] = useState(undefined);
+    const [imagePercent, setImagePercent] = useState(0);
+    const [imageError, setImageError] = useState(false);
+    const [formData, setFormData] = useState({})
+
     const {currentUser} = useSelector((state) => state.user);
-    // console.log(currentUser)
+   
+    useEffect(() => {
+        if(image) {
+            handleFileUpload(image);
+        }
+    }, [image])
+
+    const handleFileUpload = async (image) => {
+        const storage = getStorage(app);
+        const fileName = new Date().getTime() + image.name;
+        const storageRef = ref(storage, fileName);
+        const uploadTask = uploadBytesResumable(storageRef, image);
+        uploadTask.on('state_changed', (snapshot) => {
+            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            setImagePercent(Math.round(progress));
+        }),
+        (error) => {
+            setImageError(true);
+        },
+        () => {
+            getDownloadURL(uploadTask.snapshot.ref)
+            .then((downloadURL) => {
+                setFormData({ ...formData, profilePicture: downloadURL})
+            })
+        }
+    };
+
     return (
         <div className='p-3 max-w-lg mx-auto'>
             <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
             <form className='flex flex-col gap-4'>
-                 <img src={currentUser.profilePicture} alt='profile' className='h-24 w-24 self-center cursor-pointer rounded-full object-cover'/>
-                 <input defaultValue={currentUser.userName} type='text' id='userName' placeholder='Username' className='bg-slate-100 rounded-lg p-3'/>
-                 <input defaultValue={currentUser.email} type='text' id='userName' placeholder='Username' className='bg-slate-100 rounded-lg p-3'/>
-                 <input type='text' id='userName' placeholder='Username' className='bg-slate-100 rounded-lg p-3'/>
-                 <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-80'>update</button>
+                <input type='file' ref={fileRef} accept='image/*' hidden onChange={(e) => setImage(e.target.files[0])}/>
+
+                {/* 
+                Firebase Storage Rules:
+                allow read;
+                allow write: if
+                request.resource.size < 2 * 1024 * 1024 && 
+                request.resource.contentType.matches('image/.*') */}
+                <img 
+                    src={currentUser.profilePicture} 
+                    alt='profile' 
+                    className='h-24 w-24 self-center cursor-pointer rounded-full object-cover'
+                    onClick={() => fileRef.current.click()}
+                />
+                <p className='text-sm self-center'>
+                    {imageError? (
+                        <span className='text-red-700'>Error Uploading...</span>) : imagePercent > 0 && imagePercent <100 ? 
+                        ( <span className='text-slate-700'> {`Uploading: ${imagePercent}%`}</span>) : imagePercent == 100 ? 
+                        ( <span className='text-green-700'>Image uploaded Successfully</span>) : (''
+                    )}
+                </p>
+                 <input 
+                    defaultValue={currentUser.userName} 
+                    type='text' id='userName' 
+                    placeholder='Username' 
+                    className='bg-slate-100 rounded-lg p-3'
+                />
+                 <input 
+                    defaultValue={currentUser.email} 
+                    type='text' 
+                    id='email' 
+                    placeholder='Email' 
+                    className='bg-slate-100 rounded-lg p-3'
+                />
+                 <input 
+                    type='password' 
+                    id='password' 
+                    placeholder='Password' 
+                    className='bg-slate-100 rounded-lg p-3'
+                />
+                 <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-80'>
+                    update
+                </button>
             </form>
             <div className='flex justify-between mt-5'>
                 <span className='text-red-700 cursor-pointer'>Delete Account</span>
